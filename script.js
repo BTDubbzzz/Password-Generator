d30836dea05da3615afa0a0ad60de2dc435d6b84
BTDubbzzz <btdubbzzz@gmail.com>
2021-03-29 19:09:05 -0500

    wrote the conditionals for all user data gathering

--- script.js
+++ script.js
@@ -1,82 +1,95 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+ var password = generatePassword();
+ var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
-
+ passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
 
-var wantsPassword = ''
-var characterNumber = ''
-var hasLowercase = ''
-var hasUppercase = ''
-var hasNumericChars = ''
-var hasSpecialChars = ''
+var wantsPassword = '';
+var passwordLength = '';
+var hasLowercase = '';
+var hasUppercase = '';
+var hasNumericChars = '';
+var hasSpecialChars = '';
 
+var wantsGenerateAgain = '';
+var wantsSameCriteria = '';
 
-var wantsGenerateAgain = ''
-var wantsSameCriteria = ''
+var characterSet = '';
 
+const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+const numericChars = '0123456789';
+const specialChars = ' !"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
 
-
-var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz'
-var numericChars = '0123456789'
-var specialChars = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
-
-wantsPassword = confirm('Hello, would you like to generate a password?')
+wantsPassword = confirm('Hello, would you like to generate a password?');
 
 if (wantsPassword === true) {
+ definePassword();
 } else {
+ alert('Thank you');
+}
+
+function getCharacterSet() {
+ if (hasLowercase) {
+  characterSet += lowercaseChars;
+ }
+ if (hasUppercase) {
+  characterSet += uppercaseChars;
+ }
+ if (hasNumericChars) {
+  characterSet += numericChars;
+ }
+ if (hasSpecialChars) {
+  characterSet += specialChars;
+ }
+ console.log(characterSet);
+}
+
+function generatePassword() {
+ alert('generating password');
 }
 
 function getParameters() {
-  hasLowercase = confirm('Would you like to include lowercase characters?')
-  hasUppercase = confirm('Would you like to include uppercase characters?')
-  hasNumericChars = confirm('Would you like to include numeric characters?')
-  hasSpecialChars = confirm('Would you like to include special characters?')
+ hasLowercase = confirm('Would you like to include lowercase characters?');
+ hasUppercase = confirm('Would you like to include uppercase characters?');
+ hasNumericChars = confirm('Would you like to include numeric characters?');
+ hasSpecialChars = confirm('Would you like to include special characters?');
 }
 
-function getCharacterNumber() {
-  do {characterNumber = prompt('How many characters would you like your password to be? (between 8-128 characters)');
-  if (isNaN(characterNumber) || characterNumber > 128 || characterNumber < 8 ) {
-    alert('You must choose a number between 8 and 128')
+function getpasswordLength() {
+ do {
+  passwordLength = prompt(
+   'How many characters would you like your password to be? (between 8-128 characters)'
+  );
+  if (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8) {
+   alert('You must choose a number between 8 and 128');
   }
-    
-} while (isNaN(characterNumber) || characterNumber > 128 || characterNumber < 8 );
+ } while (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8);
 }
 
+function definePassword() {
+ alert('password being generated');
+ getpasswordLength();
 
-
-function generatePassword() {
-  alert('password being generated')
-  getCharacterNumber()
-
-  console.log(characterNumber)
-
-  getParameters()
-
-  if (hasLowercase || hasUppercase || hasNumericChars || hasSpecialChars) {
-    alert('generating password')
-  } else {
-    alert('must select at least 1 criteria')
-    getParameters()
+ do {
+  getParameters();
+  if (!hasLowercase && !hasUppercase && !hasNumericChars && !hasSpecialChars) {
+   alert('you must select at least 1 criteria');
   }
-  
-  
+ } while (
+  !hasLowercase &&
+  !hasUppercase &&
+  !hasNumericChars &&
+  !hasSpecialChars
+ );
 
-
-
-
-
+ getCharacterSet();
 }
-
-
-generatePassword()
