b23835b0f32bd8a42b61e5d4a060c59518ae98df
BTDubbzzz <btdubbzzz@gmail.com>
2021-03-31 00:08:50 -0500

    organized the code a bit to prepare for listeners and to comment the whole file

--- script.js
+++ script.js
@@ -21,16 +21,15 @@ generateBtn.addEventListener('click', writePassword);
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 
+// -------------------- VARIABLES INITIALIZED -------------------
 var wantsPassword = '';
 var passwordLength = '';
 var hasLowercase = '';
 var hasUppercase = '';
 var hasNumericChars = '';
 var hasSpecialChars = '';
-
 var wantsGenerateAgain = '';
 var wantsSameCriteria = '';
-
 var characterSet = '';
 var finalPasswordArray = [];
 const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -38,6 +37,28 @@ const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
 const numericChars = '0123456789';
 const specialChars = ' !"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
 var finalPassword = '';
+var randomPasswordArray = [];
+var beginningPasswordArray = [];
+
+// VARIABLES INITIALIZED
+// ------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// ------------------ GENERAL PURPOSE FUNCTIONS---------------------------------------------------
+
+//generates a random number between min and max (inclusive min exclusive max)
+function getRandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
 
 function shuffle(array) {
 	var currentIndex = array.length,
@@ -58,14 +79,27 @@ function shuffle(array) {
 
 	return array;
 }
-
-wantsPassword = confirm('Hello, would you like to generate a password?');
-// ----------------------------------------------------------------------------------------
-
-function combinePasswords() {
-	finalPasswordArray = beginningPasswordArray.concat(randomPasswordArray);
-	finalPasswordArray = shuffle(finalPasswordArray);
-	finalPassword = finalPasswordArray.join('');
+// ---------------------------------------------------------------------------------
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// ------------------------- APPLICATION FUNCTIONS --------------------------------------------------------------
+//
+function getPasswordLength() {
+	do {
+		passwordLength = prompt(
+			'How many characters would you like your password to be? (between 8-128 characters)'
+		);
+		if (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8) {
+			alert('You must choose a number between 8 and 128');
+		}
+	} while (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8);
 }
 
 function getCharacterSet() {
@@ -82,14 +116,6 @@ function getCharacterSet() {
 		characterSet += specialChars;
 	}
 }
-var randomPasswordArray = [];
-
-function getRandomPasswordArray() {
-	// generate (passwordLength - beginningPasswordArray.length) random characters from variableCharacterSet and push them to the array
-	for (let i = 0; i < passwordLength - beginningPasswordArray.length; i++) {
-		randomPasswordArray.push(getOneRandomCharacter(characterSet));
-	}
-}
 
 function getParameters() {
 	hasLowercase = confirm('Would you like to include lowercase characters?');
@@ -104,15 +130,40 @@ function getParameters() {
 	console.log('characterSet :>> ', characterSet);
 }
 
-function getPasswordLength() {
-	do {
-		passwordLength = prompt(
-			'How many characters would you like your password to be? (between 8-128 characters)'
-		);
-		if (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8) {
-			alert('You must choose a number between 8 and 128');
-		}
-	} while (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8);
+function getOneRandomCharacter(chartype) {
+	var randomNumber = getRandomNumber(0, chartype.length);
+	return chartype.charAt(randomNumber);
+}
+
+function getRandomPasswordArray() {
+	// generate (passwordLength - beginningPasswordArray.length) random characters from variableCharacterSet and push them to the array
+	for (let i = 0; i < passwordLength - beginningPasswordArray.length; i++) {
+		randomPasswordArray.push(getOneRandomCharacter(characterSet));
+	}
+}
+
+// =================================================================================================================================
+// going to pull 1 character from each chosen pool to ensure it has 1 of all chosen criteria
+
+function ensureEachCriteria() {
+	if (hasUppercase) {
+		beginningPasswordArray.push(getOneRandomCharacter(uppercaseChars));
+	}
+	if (hasLowercase) {
+		beginningPasswordArray.push(getOneRandomCharacter(lowercaseChars));
+	}
+	if (hasNumericChars) {
+		beginningPasswordArray.push(getOneRandomCharacter(numericChars));
+	}
+	if (hasSpecialChars) {
+		beginningPasswordArray.push(getOneRandomCharacter(specialChars));
+	}
+}
+
+function combinePasswords() {
+	finalPasswordArray = beginningPasswordArray.concat(randomPasswordArray);
+	finalPasswordArray = shuffle(finalPasswordArray);
+	finalPassword = finalPasswordArray.join('');
 }
 
 function definePassword() {
@@ -133,55 +184,16 @@ function definePassword() {
 		!hasNumericChars &&
 		!hasSpecialChars
 	);
+	ensureEachCriteria();
+	getRandomPasswordArray();
+	combinePasswords();
 }
 // =================================================================================================================================
-// going to pull 1 character from each chosen pool to ensure it has 1 of all chosen criteria
-
-//generates a random number between min and max (inclusive min exclusive max)
-function getRandomNumber(min, max) {
-	return Math.floor(Math.random() * (max - min) + min);
-}
-
 //
-// This gets 1 character for each chartype passed through it - will need to be saved to separate variables on each iteration
-function getOneRandomCharacter(chartype) {
-	var randomNumber = getRandomNumber(0, chartype.length);
-	return chartype.charAt(randomNumber);
-}
+wantsPassword = confirm('Hello, would you like to generate a password?');
 
-console.log(
-	'getOneRandomCharacter(uppercaseChars) :>> ',
-	getOneRandomCharacter(uppercaseChars)
-);
-
-var beginningPasswordArray = [];
-
-function ensureEachCriteria() {
-	if (hasUppercase) {
-		beginningPasswordArray.push(getOneRandomCharacter(uppercaseChars));
-	}
-	if (hasLowercase) {
-		beginningPasswordArray.push(getOneRandomCharacter(lowercaseChars));
-	}
-	if (hasNumericChars) {
-		beginningPasswordArray.push(getOneRandomCharacter(numericChars));
-	}
-	if (hasSpecialChars) {
-		beginningPasswordArray.push(getOneRandomCharacter(specialChars));
-	}
-}
-
-// =================================================================================================================================
 if (wantsPassword === true) {
-	definePassword();
+	definePassword(); // FINAL PASSWORD ALERT OR WRITE-TO-PAGE INSIDE THIS IF ELSE (IF THEY CANCEL THE PROGRAM DO NOT WRITE NULL PASSWORD)
 } else {
 	alert('Thank you');
 }
-
-ensureEachCriteria();
-getRandomPasswordArray();
-combinePasswords();
-console.log('beginningPasswordArray :>> ', beginningPasswordArray);
-console.log('randomPasswordArray :>> ', randomPasswordArray);
-console.log('finalPasswordArray :>> ', finalPasswordArray);
-console.log('finalPassword :>> ', finalPassword);
