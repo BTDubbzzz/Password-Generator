93663ef6b4cec91481c281d2123a7fed1f356bbe
BTDubbzzz <btdubbzzz@gmail.com>
2021-03-30 23:34:25 -0500

    completed the password generator, just needs cleaning up and organizing

--- script.js
+++ script.js
@@ -32,14 +32,64 @@ var wantsGenerateAgain = '';
 var wantsSameCriteria = '';
 
 var characterSet = '';
-
+var finalPasswordArray = [];
 const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
 const numericChars = '0123456789';
 const specialChars = ' !"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
+var finalPassword = '';
+
+function shuffle(array) {
+	var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
 
 wantsPassword = confirm('Hello, would you like to generate a password?');
 // ----------------------------------------------------------------------------------------
+
+function combinePasswords() {
+	finalPasswordArray = beginningPasswordArray.concat(randomPasswordArray);
+	finalPasswordArray = shuffle(finalPasswordArray);
+	finalPassword = finalPasswordArray.join('');
+}
+
+function getCharacterSet() {
+	if (hasUppercase) {
+		characterSet += uppercaseChars;
+	}
+	if (hasLowercase) {
+		characterSet += lowercaseChars;
+	}
+	if (hasNumericChars) {
+		characterSet += numericChars;
+	}
+	if (hasSpecialChars) {
+		characterSet += specialChars;
+	}
+}
+var randomPasswordArray = [];
+
+function getRandomPasswordArray() {
+	// generate (passwordLength - beginningPasswordArray.length) random characters from variableCharacterSet and push them to the array
+	for (let i = 0; i < passwordLength - beginningPasswordArray.length; i++) {
+		randomPasswordArray.push(getOneRandomCharacter(characterSet));
+	}
+}
 
 function getParameters() {
 	hasLowercase = confirm('Would you like to include lowercase characters?');
@@ -50,6 +100,8 @@ function getParameters() {
 	console.log('hasNumericChars :>> ', hasNumericChars);
 	hasSpecialChars = confirm('Would you like to include special characters?');
 	console.log('hasSpecialChars :>> ', hasSpecialChars);
+	getCharacterSet();
+	console.log('characterSet :>> ', characterSet);
 }
 
 function getPasswordLength() {
@@ -85,6 +137,7 @@ function definePassword() {
 // =================================================================================================================================
 // going to pull 1 character from each chosen pool to ensure it has 1 of all chosen criteria
 
+//generates a random number between min and max (inclusive min exclusive max)
 function getRandomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
@@ -126,4 +179,9 @@ if (wantsPassword === true) {
 }
 
 ensureEachCriteria();
+getRandomPasswordArray();
+combinePasswords();
 console.log('beginningPasswordArray :>> ', beginningPasswordArray);
+console.log('randomPasswordArray :>> ', randomPasswordArray);
+console.log('finalPasswordArray :>> ', finalPasswordArray);
+console.log('finalPassword :>> ', finalPassword);
